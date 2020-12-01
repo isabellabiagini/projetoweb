71799dc19e0ff7d460a89f65d218f1aff11e1123
isabellabiagini <isabella.costa@bandtec.com.br>
2020-12-01 01:29:31 -0300

    arrumando a meleca que eu fiz

--- ac3-isabellabiagini/src/main/java/br/com/bandtec/ac3isabellabiagini/repository/DoceRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.bandtec.ac3isabellabiagini.repository;
-
-import br.com.bandtec.ac3isabellabiagini.classes.Doce;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoceRepository extends JpaRepository<Doce, Integer> {
-}
